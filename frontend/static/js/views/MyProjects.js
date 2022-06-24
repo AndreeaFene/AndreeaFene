7dcf2d3b259b247aa669b46d19bad335318c014c
Fenesanu Andreea <bogdana_fene@yahoo.com>
2022-06-24 22:58:47 +0300

    modified relative paths of images to direct path

--- frontend/static/js/views/MyProjects.js
+++ frontend/static/js/views/MyProjects.js
@@ -14,27 +14,27 @@ export default class extends AbstractView {
             <div class="MyProjectStyle">
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/01%20week/my%20blog" download>Blog Home Page</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home1_1.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home1_1.PNG alt="Andreea">
             </div>
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/06_week_homework" download>Player Box Game</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home3.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home3.PNG" alt="Andreea">
             </div>
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/11_week_homework" download>Avoiding Monsters Game</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home6.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home6.PNG" alt="Andreea">
             </div>
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/07_week_homework" download>Stopwatch App</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home4.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home4.PNG" alt="Andreea">
             </div>
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/10_week_homework" download>Spaceship Generator</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home5.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home5.PNG" alt="Andreea">
             </div>
             <div class="SubProject">
             <p><a href="https://github.com/AndreeaFene/SIIT_Andreea_Fenesanu/tree/main/optional_week_2" download>Simple Dog Breeds Blog Page</a></p>
-            <img id="ImgMyProjects" src="frontend/static/images/home7.png" alt="Andreea">
+            <img id="ImgMyProjects" src="https://andreeafene.github.io/AndreeaFene/frontend/static/images/home7.PNG" alt="Andreea">
             </div>
             <footer>
             <span id = "footer-content">Designed by Andreea Fenesanu</span>
